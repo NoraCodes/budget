d3e9955a5b7e9adaf635ffc90d965e2247e7b6dc
SilverWingedSeraph <lfstindall@gmail.com>
2016-06-05 17:31:47 -0700

    Starting up.

--- budget/src/main.rs
+++ budget/src/main.rs
@@ -1,3 +1,9 @@
+
 fn main() {
-    println!("Hello, world!");
+    use std::env;
+    // Collect arguments into a vector
+    let mut arguments = Vec::new();
+    for argument in env::args() {
+        arguments.push(argument);
+    }
 }
