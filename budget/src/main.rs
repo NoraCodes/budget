34f19b1bcdf9b1433241335cb1622e640ec97f64
SilverWingedSeraph <lfstindall@gmail.com>
2016-06-06 20:00:39 -0700

    Set up test executable

--- budget/src/main.rs
+++ budget/src/main.rs
@@ -1,3 +1,12 @@
+extern crate time;
+extern crate rustc_serialize;
+
+use rustc_serialize::json;
+
+mod transaction;
+mod account;
+
+use account::*;
 
 fn main() {
     use std::env;
@@ -6,4 +15,16 @@ fn main() {
     for argument in env::args() {
         arguments.push(argument);
     }
+    let t = Transaction {
+        amount: 100,
+        recur_day: None,
+        complete: true,
+    };
+
+    println!("{:?}", &t);
+    let enc = json::encode(&t).unwrap();
+    println!("{}", &enc);
+    let dec: Transaction = json::decode(&enc).unwrap();
+    println!("{:?}", dec);
 }
+
