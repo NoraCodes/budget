30153ca565a32fe6cbd6468200a5cc2d143b1baf
SilverWingedSeraph <lfstindall@gmail.com>
2016-06-06 20:00:11 -0700

    Add Account and Transaction structs

---
+++ budget/src/account.rs
@@ -0,0 +1,26 @@
+extern crate time;
+pub use transaction::Transaction;
+
+#[derive(Debug, RustcDecodable, RustcEncodable)]
+pub struct Account {
+    /// The root recurring payment for this account.
+    /// This is the date on which the account resets.
+    pub payday: Transaction,
+    /// These are the transactions for the account, historical
+    /// and scheduled. Because scheduling is based on day of the month,
+    /// you cannot schedule one-time transactions.
+    pub transactions: Vec<Transaction>,
+    /// The account's balance, in the currency's minimal unit (i.e., cents). 
+    /// If negative, the account is in debt.
+    pub balance: i64,
+}
+
+impl Account {
+    fn apply_transaction(&mut self, trans: &mut Transaction) {
+        // Apply the transaction to us
+        self.balance += trans.amount;
+        // And let the transaction know it's been applied
+        trans.complete = true;
+    }
+
+}
---
+++ budget/src/transaction.rs
@@ -0,0 +1,15 @@
+extern crate rustc_serialize;
+use rustc_serialize::json;
+
+/// This struct represents a transaction, either recurring or one-time, which can be applied to an
+/// account, either adding to or subtracting from the balance of that account
+#[derive(Debug, RustcDecodable, RustcEncodable)]
+pub struct Transaction {
+    /// The amount of the transaction, positive for a deposit, negative for a withdrawl
+    pub amount: i64,
+    /// The day of the month on which this transaction occurs
+    pub recur_day: Option<u8>,
+    /// Whether or not this transaction has been completed this month
+    /// The payday transaction
+    pub complete: bool,
+}
